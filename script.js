70ca9355d49aa2d96f89afcca157fc3541411480
Cameline Njoroge <camelinejoroge@gmail.com>
2024-02-28 18:09:04 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu').onclick =() =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cartItem.classList.remove('active');
+
+}
+let search = document.querySelector('.search-form');
+
+document.querySelector('#search').onclick =() =>{
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    
+}
+let cartItem = document.querySelector('.cart-item-container');
+
+document.querySelector('#cart').onclick =() =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+    
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+    cartItem.classList.remove('active');
+}
